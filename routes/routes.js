108ff903118ed4e0a0c078a69ca4ffdc689e204a
Sandeep-1405 <sandeepmaraboina14s@gmail.com>
2024-12-11 15:23:47 +0530

    Implemented SignUp

---
+++ routes/routes.js
@@ -0,0 +1,17 @@
+const router = require('express').Router()
+
+const {
+    createUser,
+    userLogin,
+    authentication,
+
+    
+} = require('../controllers/controllers')
+
+router.post('/register', createUser);
+
+router.post('/login', userLogin);
+
+
+
+module.exports = router
